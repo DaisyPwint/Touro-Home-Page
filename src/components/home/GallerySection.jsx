afea07891ec5249b1beb03656a41eed69ba57de8
phoopwint <phoopwint445@gmail.com>
2024-01-10 15:50:28 +0630

    modified: gallery section

--- src/components/home/GallerySection.jsx
+++ src/components/home/GallerySection.jsx
@@ -3,12 +3,12 @@ import Title from "../Title";
 export default function GallerySection() {
 
     const galleryItems = [
-    { imageSrc: 'https://img.freepik.com/free-photo/woman-with-hat-sitting-chairs-beach-beautiful-tropical-beach-woman-relaxing-tropical-beach-koh-nangyuan-island_335224-1110.jpg?size=626&ext=jpg&ga=GA1.2.626863243.1702211119&semt=sph', height: { md: '400px', sm: '250px', base: '200px' } },
-    { imageSrc: 'https://img.freepik.com/free-photo/maldives-island_74190-2340.jpg?size=626&ext=jpg&ga=GA1.1.626863243.1702211119&semt=sph', height: { md: '250px', sm: '150px', base: '100px' } },
-    { imageSrc: 'https://img.freepik.com/free-photo/woman-hand-holding-camera-standing-top-rock-nature-travel-concept_335224-887.jpg?size=626&ext=jpg&ga=GA1.2.626863243.1702211119&semt=sph', height: { md: '250px', sm: '150px', base: '100px' } },
-    { imageSrc: 'https://img.freepik.com/free-photo/group-man-woman-enjoy-camping-picnic-barbecue-lake-with-tents-background-young-mixed-race-asian-woman-man-young-people-s-hands-toasting-cheering-bottles-beer_1253-1041.jpg?size=626&ext=jpg&ga=GA1.1.626863243.1702211119&semt=sph', height: { md: '400px', sm: '250px', base: '200px' } },
-    { imageSrc: 'https://img.freepik.com/free-photo/traveler-asian-woman-traveling-walking-bangkok-thailand_7861-1292.jpg?size=626&ext=jpg&ga=GA1.2.626863243.1702211119&semt=sph', height: { md: '400px', sm: '250px', base: '200px' } },
-    { imageSrc: 'https://img.freepik.com/free-photo/wooden-bridge-koh-nangyuan-island-surat-thani-thailand_335224-1082.jpg?size=626&ext=jpg&ga=GA1.2.626863243.1702211119&semt=sph', height: { md: '250px', sm: '150px', base: '100px' } },
+    { imageSrc: 'https://img.freepik.com/free-photo/woman-with-hat-sitting-chairs-beach-beautiful-tropical-beach-woman-relaxing-tropical-beach-koh-nangyuan-island_335224-1110.jpg?size=626&ext=jpg&ga=GA1.2.626863243.1702211119&semt=sph', styleClass: 'md:h-[400px] sm:h-[250px] h-[200px]' },
+    { imageSrc: 'https://img.freepik.com/free-photo/maldives-island_74190-2340.jpg?size=626&ext=jpg&ga=GA1.1.626863243.1702211119&semt=sph', styleClass: 'md:h-[250px] sm:h-[150px] h-[100px]'},
+    { imageSrc: 'https://img.freepik.com/free-photo/woman-hand-holding-camera-standing-top-rock-nature-travel-concept_335224-887.jpg?size=626&ext=jpg&ga=GA1.2.626863243.1702211119&semt=sph', styleClass: 'md:h-[250px] sm:h-[150px] h-[100px]'},
+    { imageSrc: 'https://img.freepik.com/free-photo/group-man-woman-enjoy-camping-picnic-barbecue-lake-with-tents-background-young-mixed-race-asian-woman-man-young-people-s-hands-toasting-cheering-bottles-beer_1253-1041.jpg?size=626&ext=jpg&ga=GA1.1.626863243.1702211119&semt=sph', styleClass: 'md:h-[400px] sm:h-[250px] h-[200px]'},
+    { imageSrc: 'https://img.freepik.com/free-photo/traveler-asian-woman-traveling-walking-bangkok-thailand_7861-1292.jpg?size=626&ext=jpg&ga=GA1.2.626863243.1702211119&semt=sph', styleClass: 'md:h-[400px] sm:h-[250px] h-[200px]'},
+    { imageSrc: 'https://img.freepik.com/free-photo/wooden-bridge-koh-nangyuan-island-surat-thani-thailand_335224-1082.jpg?size=626&ext=jpg&ga=GA1.2.626863243.1702211119&semt=sph', styleClass: 'md:h-[250px] sm:h-[150px] h-[100px]'},
   ];
 
   return (
@@ -40,11 +40,12 @@ export default function GallerySection() {
 }
 
 const GalleryItem = ({ img }) => {
-  const {imageSrc,height} = img;
+  const {imageSrc,styleClass} = img;
+
   return (
   
     <div className="flex flex-col gap-4">
-      <div className={`md:h-[${height.md}] sm:h-[${height.sm}] h-[${height.base}]`}>
+      <div className={styleClass}>
         <img className="block h-full w-full rounded-lg object-cover object-center" src={imageSrc} alt="gallery img" />
       </div>
     </div>
